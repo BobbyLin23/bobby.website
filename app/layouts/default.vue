55583d5ec702065e8773149c641a84ba54097a11
BobbyLin <linzhangsheng23@gmail.com>
2025-07-18 21:11:01 +0800

    feat: add basic layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="sm:border-x mx-auto max-w-[var(--ui-container)] min-h-screen border-default px-4 sm:px-6 lg:px-8 pt-10">
     <slot />
   </div>
 </template>
