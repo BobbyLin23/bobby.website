78e15ec7df7b022f306ea21e8bb9f61d1cf4f53d
bobbylin23 <linzhangsheng23@gmail.com>
2025-07-18 15:06:27 +0800

    chore: add cursor rules and mcp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
-import tailwindcss from '@tailwindcss/vite'
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui', '@nuxt/content', 'motion-v/nuxt'],
+  modules: [
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    'motion-v/nuxt',
+    'nuxt-mcp',
+  ],
   css: ['~/assets/css/main.css'],
   ui: {
     fonts: false,
