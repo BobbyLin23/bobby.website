44db0e9305d00f16cac3741ce860984b9b1a403c
Bobby Lin <linzhangsheng23@gmail.com>
2025-07-20 11:40:13 +0800

    feat: add footer (#5)

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { footer } = useAppConfig()
+</script>
+
+<template>
+  <div
+    class="z-10 bg-default text-xs flex justify-between items-center"
+  >
+    <span>
+      {{ footer.credits }}
+    </span>
+
+    <div class="flex gap-2">
+      <template v-if="footer?.links">
+        <UButton
+          v-for="(link, index) of footer?.links"
+          :key="index"
+          v-bind="{ size: 'xs', color: 'neutral', variant: 'ghost', ...link }"
+        />
+      </template>
+    </div>
+  </div>
+</template>
