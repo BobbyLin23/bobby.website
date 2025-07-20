13f1f356a8440db565b4573f0b010024b1c6f133
Bobby Lin <linzhangsheng23@gmail.com>
2025-07-20 17:38:24 +0800

    feat: add home page hero (#6)

---
+++ app/components/Hero.vue
@@ -0,0 +1,115 @@
+<script lang="ts" setup>
+import type { IndexCollectionItem } from '@nuxt/content'
+
+defineProps<{
+  page: IndexCollectionItem
+}>()
+
+const { footer } = useAppConfig()
+</script>
+
+<template>
+  <div class="text-center">
+    <div class="flex items-center justify-center mb-4">
+      <Motion
+        :initial="{
+          scale: 1.1,
+          opacity: 0,
+          filter: 'blur(20px)',
+        }"
+        :animate="{
+          scale: 1,
+          opacity: 1,
+          filter: 'blur(0px)',
+        }"
+        :transition="{
+          duration: 0.6,
+          delay: 0.1,
+        }"
+      >
+        <UAvatar
+          class="size-18 ring ring-default ring-offset-3 ring-offset-(--ui-bg)"
+          src="/image.png"
+        />
+      </Motion>
+    </div>
+    <h1 class="tracking-tight font-bold text-pretty text-3xl sm:text-4xl lg:text-5xl text-shadow-md max-w-lg mx-auto">
+      <div class="text-shadow-md max-w-lg mx-auto">
+        <Motion
+          :initial="{
+            scale: 1.1,
+            opacity: 0,
+            filter: 'blur(20px)',
+          }"
+          :animate="{
+            scale: 1,
+            opacity: 1,
+            filter: 'blur(0px)',
+          }"
+          :transition="{
+            duration: 0.6,
+            delay: 0.1,
+          }"
+        >
+          {{ page.title }}
+        </Motion>
+      </div>
+    </h1>
+    <div class="text-md mx-auto max-w-2xl sm:text-md text-muted text-balance mt-6">
+      <Motion
+        :initial="{
+          scale: 1.1,
+          opacity: 0,
+          filter: 'blur(20px)',
+        }"
+        :animate="{
+          scale: 1,
+          opacity: 1,
+          filter: 'blur(0px)',
+        }"
+        :transition="{
+          duration: 0.6,
+          delay: 0.3,
+        }"
+      >
+        {{ page.description }}
+      </Motion>
+    </div>
+    <div class="gap-x-4 inline-flex mt-4">
+      <Motion
+        v-for="(link, index) of footer?.links"
+        :key="index"
+
+        :initial="{
+          scale: 1.1,
+          opacity: 0,
+          filter: 'blur(20px)',
+        }"
+        :animate="{
+          scale: 1,
+          opacity: 1,
+          filter: 'blur(0px)',
+        }"
+        :transition="{
+          duration: 0.6,
+          delay: 0.5 + index * 0.1,
+        }"
+      >
+        <UButton
+          v-bind="{ size: 'md', color: 'neutral', variant: 'ghost', ...link }"
+        />
+      </Motion>
+    </div>
+    <div class="mt-8">
+      <UCarousel
+        v-slot="{ item }"
+        loop
+        :autoplay="{ delay: 2000 }"
+        :items="page.hero.images"
+        :ui="{ item: 'basis-1/4', container: 'w-full' }"
+      >
+        <img :src="item.src" :alt="item.alt" width="234" height="234" class="rounded-lg">
+      </UCarousel>
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,28 @@
 <script lang="ts" setup>
+const { data: page } = await useAsyncData('index', () => {
+  return queryCollection('index').first()
+})
+if (!page.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page not found',
+    fatal: true,
+  })
+}
 
+useSeoMeta({
+  title: page.value?.seo.title || page.value?.title,
+  ogTitle: page.value?.seo.title || page.value?.title,
+  description: page.value?.seo.description || page.value?.description,
+  ogDescription: page.value?.seo.description || page.value?.description,
+})
 </script>
 
 <template>
-  <div class="text-red-500">
-    <h1>Hello World</h1>
+  <div
+    v-if="page"
+    class="w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 flex flex-col lg:grid gap-16 sm:gap-y-24 py-18 sm:py-24 lg:py-32"
+  >
+    <Hero :page="page" />
   </div>
 </template>
