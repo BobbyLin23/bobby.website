9fd1b7eb9a440acd1005e412aa5a63ae544c234e
BobbyLin <linzhangsheng23@gmail.com>
2025-07-20 11:39:49 +0800

    feat: add footer

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,4 +5,22 @@ export default defineAppConfig({
       neutral: 'neutral',
     },
   },
+  footer: {
+    credits: `Copyright © ${new Date().getFullYear()}`,
+    colorMode: false,
+    links: [
+      {
+        'icon': 'i-simple-icons-x',
+        'to': 'https://x.com/BobbyLin23',
+        'target': '_blank',
+        'aria-label': 'BobbyLin23 on X',
+      },
+      {
+        'icon': 'i-simple-icons-github',
+        'to': 'https://github.com/BobbyLin23',
+        'target': '_blank',
+        'aria-label': 'BobbyLin23 on GitHub',
+      },
+    ],
+  },
 })
